8e9b4e68fd1c9c3cec24b00211429c5dc10a4ffa
unknown <dharmamech3695@gmail.com>
2020-06-16 16:40:23 +0530

    pac-json-file and client jsfile modified

--- static/js/weatherReport.js
+++ static/js/weatherReport.js
@@ -8,14 +8,15 @@ const temperature = document.querySelector("#weather-temperature");
 const feelslike = document.querySelector("#weather-feelslike");
 const windDir = document.querySelector("#weather-windDir");
 const rain_chances = document.querySelector("#rain-chances");
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let location = searchInput.value
     weather_location.textContent = 'loading ....'
-    fetch(`http://localhost:3000/weather?location=${location}`).then((response) => {
+    fetch(`/weather?location=${location}`).then((response) => {
         response.json().then((data) => {
-            
-                if(!data.error){
+
+            if (!data.error) {
                 weather_location.textContent = `Your Current Location :${data.Current_Location}`;
                 description.textContent = `Current Weather : ${data.Weather_Description}`;
                 temperature.textContent = `Temperature : ${data.Temperature}`;
@@ -23,16 +24,16 @@ form.addEventListener('submit', (e) => {
                 feelslike.textContent = `Feels like : ${data.Feels_Like}`;
 
                 windDir.textContent = `Wind Direction : ${data.Wind_Direction}`;
-                } else if( data.error) {
-                    temperature.textContent = `${data.error}` 
-                    weather_location.textContent='';
-                    description.textContent='';
-                    feelslike.textContent='';
-                    windDir.textContent=''
+            } else if (data.error) {
+                temperature.textContent = `${data.error}`
+                weather_location.textContent = '';
+                description.textContent = '';
+                feelslike.textContent = '';
+                windDir.textContent = ''
 
-                }
-            }).catch((error) => {
-                    console.log(error)
-            })
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
     })
 })
